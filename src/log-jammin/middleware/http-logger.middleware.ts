040d0bb981a2880e4416ecbd68033615c275ee52
Michael Kelly <mike@curioushuman.com.au>
2022-03-06 11:49:54 +1100

    Minor typo fix

--- src/log-jammin/middleware/http-logger.middleware.ts
+++ src/log-jammin/middleware/http-logger.middleware.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
-export class LoggerMiddleware implements NestMiddleware {
+export class HttpLoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
