2676899f20f9ad70e59f7c203fa33355ea690b1d
Michael Kelly <mike@curioushuman.com.au>
2022-03-22 09:12:31 +1100

    common-config: erradicating issues

--- src/common-config/databases/database.ts
+++ src/common-config/databases/database.ts
@@ -26,13 +26,16 @@ export enum Rdbms {
 export type DatabaseConfigGroup = CommonConfigGroup<Databases, MongoDb | Rdbms>;
 
 function mongoUri() {
+  console.log('process.env', process.env);
   const appName = process.env.RBC_APP_NAME;
   const releaseName = process.env.RBC_RELEASE_NAME;
   const dbSvcName = process.env.RBC_DATABASE_SVC_NAME;
   const dbName = process.env.RBC_DATABASE_NAME || appName;
   const dbPort = process.env.RBC_DATABASE_NAME || appName;
-  const host = process.env[`${releaseName}_${dbSvcName}_SERVICE_HOST`];
-  return `mongodb://${host}:${dbPort}/${dbName}`;
+  const hostEnv = `${releaseName}_${dbSvcName}_SERVICE_HOST`;
+  console.log('hostEnv', hostEnv);
+  const dbHost = process.env[hostEnv];
+  return `mongodb://${dbHost}:${dbPort}/${dbName}`;
 }
 
 export function mongoDbConfig(): CommonConfigKeyValue<MongoDb> {
